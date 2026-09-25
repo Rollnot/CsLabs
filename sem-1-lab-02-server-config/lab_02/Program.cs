82af229207a4026478af6582f397505b4c55e0ad
Rollnot <pudik174@gmail.com>
2026-09-25 19:52:47 +0500

    added sem-1-lab-02-server-config

---
+++ sem-1-lab-02-server-config/lab_02/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace lab_02;
+
+public static class Program
+{
+
+    public static string CheckConfiguration(int ram, int players, bool start, bool password) 
+    {
+        if (start == true)
+        {
+            if (players == 0)
+            {
+                return "[ERROR] Количество игроков должно быть больше нуля.";
+            }
+            if (ram < 2)
+            {
+                return "[ERROR] Серверу недостаточно оперативной памяти.";
+            }
+            if (players/ram >= 8)
+            {
+                Console.WriteLine("[WARNING] Для такого количества игроков рекомендуется больше оперативной памяти.");
+            }
+            if (password == true)
+            {
+                Console.WriteLine("[WARNING] Сервер защищён паролем.");
+            }
+            return "[INFO] Сервер запущен.";
+        }
+        else 
+        {
+            return "[INFO] Отмена запуска сервера...";
+        }
+    }
+
+    public static string Password(string PasswordInput)
+    {
+        if (PasswordInput == "y")
+        {
+            return "true";
+        }
+        else
+        {
+            return "false";
+        } 
+    }
+
+    public static string Start(string StartInput)
+    {
+        if (StartInput == "y")
+        {
+            return "true";
+        }
+        else
+        {
+            return "false";
+        } 
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Введите количество оперативной памяти (в ГБ): ");
+        string RamInput = Console.ReadLine();
+        int ram = Convert.ToInt32(RamInput);
+
+        Console.Write("Введите количество игроков: ");
+        string PlayersInput = Console.ReadLine();
+        int players = Convert.ToInt32(PlayersInput);
+
+        Console.Write("Установить пароль? [y/N] ");
+        string PasswordInput = Console.ReadLine();
+        bool password = Convert.ToBoolean(Password(PasswordInput));
+
+        Console.WriteLine(
+            $"""
+
+            Оперативная память: {ram}ГБ
+            Игроки: {players}
+            Пароль: {password}
+
+            """);
+
+        Console.Write("Запустить сервер? [y/N] ");
+        string StartInput = Console.ReadLine();
+        bool start = Convert.ToBoolean(Start(StartInput));
+
+
+        Console.WriteLine(CheckConfiguration(ram, players, start, password));
+    }
+}
---
+++ sem-1-lab-02-server-config/tests/UnitTest1.cs
@@ -0,0 +1,69 @@
+﻿namespace tests;
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using lab_02;
+using NUnit.Framework;
+
+public class Tests
+{
+    [Test]
+    public void CheckConfiguration_ValidConfiguration_ServerIsReady()
+    {
+        var result = Program.CheckConfiguration(8, 50, true, false);
+
+        Assert.That(result, Is.EqualTo("[INFO] Сервер запущен."));
+    }
+
+    [Test]
+    public void CheckConfiguration_ZeroPlayers_LaunchIsImpossible()
+    {
+        var result = Program.CheckConfiguration(8, 0, true, false);
+
+        Assert.That(
+            result,
+            Is.EqualTo("[ERROR] Количество игроков должно быть больше нуля."));
+    }
+
+    [Test]
+    public void CheckConfiguration_NotEnoughMemory_LaunchIsImpossible()
+    {
+        var result = Program.CheckConfiguration(1, 50, true, false);
+
+        Assert.That(
+            result,
+            Is.EqualTo("[ERROR] Серверу недостаточно оперативной памяти."));
+    }
+
+    // [Test]
+    // public void CheckConfiguration_PublicServerWithPassword_LaunchWithWarning()
+    // {
+    //     var result = Program.CheckConfiguration(8, 50, true, true);
+// 
+//     //     Assert.That(
+//     //         result,
+//     //         Is.EqualTo("[WARNING] Сервер защищён паролем."));
+//     // }
+// 
+//     // [Test]
+//     // public void CheckConfiguration_TooManyPlayersForAvailableMemory_LaunchWithWarning()
+//     // {
+//     //     var result = Program.CheckConfiguration(4, 32, true, false);
+// 
+//     //     Assert.That(
+    //         result,
+    //         Is.EqualTo(
+    //             "[WARNING] Для такого количества игроков рекомендуется больше оперативной памяти."));
+    // }
+
+    [Test]
+    public void CheckConfiguration_InvalidPlayersAndPublicServerWithPassword_LaunchIsImpossible()
+    {
+        var result = Program.CheckConfiguration(8, 0, true, true);
+
+        Assert.That(
+            result,
+            Is.EqualTo("[ERROR] Количество игроков должно быть больше нуля."));
+    }
+}
